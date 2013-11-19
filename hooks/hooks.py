9e4563a0b1de746087c227dffca3af7ab48c5ba1
InformatiQ <rhanna@informatiq.org>
2013-11-19 12:31:15 +0200

    add charmhelpers lib and typos

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -1,11 +1,21 @@
+#!/usr/bin/env python
+    
+import sys
+import charmhelpers.contrib.ansible
+
 # Create the hooks helper, passing a list of hooks which will be
 # handled by default by running all sections of the playbook
 # tagged with the hook name.
 hooks = charmhelpers.contrib.ansible.AnsibleHooks(
-    playbook_path='playbooks/site.yaml',
+    playbook_path='playbooks/sites.yml',
     default_hooks=['start', 'stop', 'config-changed',
                    'solr-relation-changed'])
- 
+
 @hooks.hook()
 def install():
     charmhelpers.contrib.ansible.install_ansible_support(from_ppa=True)
+
+
+if __name__ == "__main__":
+        hooks.execute(sys.argv)
+
